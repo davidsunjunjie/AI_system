4a8258ea48c92324c4d9e22497ce96442126553a
davidsunjunjie <davidsunjunjie@gmail.com>
2023-10-29 19:38:36 -0700

    OK it works but still have bugs

--- Computer_vision.py
+++ Computer_vision.py
@@ -12,10 +12,10 @@ print("Once started press 'q' to quit.")
 
 # keyboard.wait('s')
 
-
+#initial condition here
 left = True # set this to be false to detect the right side 
-x= 340
-y =850
+x= 940
+y= 788
 
 sct = mss.mss()
 
@@ -35,12 +35,12 @@ dimensions_right = {
 
 scr_l = numpy.array(sct.grab(dimensions_left))
 scr_r = numpy.array(sct.grab(dimensions_right))
-# # button shows up and there is left and right is tuned 
+# button shows up and there is left and right is tuned 
 cv2.imshow('Captured Screen Left', scr_l)
 cv2.imshow('Captured Screen Right',scr_r)
 
 cv2.waitKey(0)
-#capture works 
+# #capture works 
 
 
 wood_left = cv2.imread('woodleft.jpg')
@@ -53,10 +53,12 @@ h = wood_left.shape[0]
 fps_time = time()
 
 while True:
+    # initial condition;
 
     if left:
         scr = numpy.array(sct.grab(dimensions_left))
         wood = wood_left
+        
     else:
         scr = numpy.array(sct.grab(dimensions_right))
         wood = wood_right
@@ -67,46 +69,46 @@ while True:
     result = cv2.matchTemplate(scr_remove,wood,cv2.TM_CCOEFF_NORMED)
 
     _,max_val,_,max_loc = cv2.minMaxLoc(result)
-
-    if max_val > .84:# adjust the threshold based on your settings 
-        
-        cv2.rectangle(scr, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
-        cv2.imshow('Match Found',scr)
-        cv2.waitKey(1)
-    else: 
-        print("Nothing")
-
-
     print(f"Max Val: {max_val} Max Loc: {max_loc}")
+    src =scr.copy()
 
-    # with open('output.txt', 'a') as f:
-    #     f.write(f"Max Val: {max_val} Max Loc: {max_loc}\n")
+    if max_val > .75:# adjust the threshold based on your settings 
+        # the logic here should be easy like when it detect no matter from woodleft or woodright 
+        # then it will move the mouse to opposite and then click        
+        # it could be from left or right here we need to think about 
+
+        left = not left
+        if left:
+            x=690
+        else:
+            x=940
+    
+        cv2.rectangle(scr, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
+
+
+    cv2.imshow('Screen Shot', scr)
+    cv2.waitKey(1)
+    pyautogui.click(x=x, y=y)
+    sleep(0.5)
 
 
     if keyboard.is_pressed('q'):
         break
 
-    # src = scr.copy()
-    
-    # if max_val > .85:
-    #     left = not left
-    #     if left:
-    #         x=660
-    #     else:
-    #         x=910
-    #     cv2.rectangle(scr, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
-
-    # cv2.imshow('Screen Shot', scr)
-    # cv2.waitKey(1)
-
-    # pyautogui.click(x=x, y=y)
-    
-    # sleep(.10)
-    # fps_time = time()
-    
-#     if keyboard.is_pressed('q'):
-#         break
-
-    
     # print('FPS: {}'.format(1 / (time() - fps_time)))
-    
+    fps_time = time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-
+scr_l = numpy.array(sct.grab(dimensions_left))
+# scr_r = numpy.array(sct.grab(dimensions_right))
